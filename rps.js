31cd70d2d8bac56c7b09fd5ae97ddc91d2b48f71
Revolution5 <damonboone99@gmail.com>
2022-03-29 16:33:40 -0700

    Add function to play 5 rounds and keep score

--- rps.js
+++ rps.js
@@ -4,8 +4,7 @@ function computerPlay() {
     return randomChoice;
 }
 
-let playerChoice = window.prompt("Enter Rock, Paper, or Scissors: ");
-let computerChoice = computerPlay();
+
 
 function playRound(playerSelection, computerSelection) {
     playerSelection.toLowerCase();
@@ -15,32 +14,62 @@ function playRound(playerSelection, computerSelection) {
 
     switch(true) {
         case playerSelection == "rock" && computerSelection == "rock":
-            return "It's a tie!";
+            console.log("It's a tie!");
+            return 0;
             break;
         case playerSelection == "rock" && computerSelection == "paper":
-            return "You lost! Paper beats rock";
+            console.log("You lost! Paper beats rock");
+            return 0;
             break;
         case playerSelection == "rock" && computerSelection == "scissors":
-            return "You Won! Rock beats scissors";
+            console.log("You Won! Rock beats scissors");
+            return 1;
             break;
         case playerSelection == "paper" && computerSelection == "rock":
-            return "You Won! Paper beats rock";
+            console.log("You Won! Paper beats rock");
+            return 1;
             break;
         case playerSelection == "paper" && computerSelection == "paper":
-            return "It's a tie!";
+            console.log("It's a tie!");
+            return 0;
             break;
         case playerSelection == "paper" && computerSelection == "scissors":
-            return "You lost! Scissors beat paper";
+            console.log("You lost! Scissors beat paper");
+            return 0;
             break;
         case playerSelection == "scissors" && computerSelection == "rock":
-            return "You lost! Rock beats scissors";
+            console.log("You lost! Rock beats scissors");
+            return 0;
             break;
         case playerSelection == "scissors" && computerSelection == "paper":
-            return "You won! Scissors beat paper";
+            console.log("You won! Scissors beat paper");
+            return 1;
             break;
         case playerSelection == "scissors" && computerSelection == "scissors":
-            return "It's a tie!";
+            console.log("It's a tie!");
+            return 0;
             break;
     }
 }
-console.log(playRound(playerChoice, computerChoice))
+
+function game() {
+    let score = 0;
+
+    for(let i = 0; i < 5; i++) {
+        let playerChoice = window.prompt("Enter Rock, Paper, or Scissors: ");
+        let computerChoice = computerPlay();
+
+        result = playRound(playerChoice, computerChoice);
+        score += result;
+        console.log("Current score: " + score + "\n");
+    }
+
+    if (score >= 3) {
+        console.log("You won the game!");
+    }
+    else {
+        console.log("You lost the game! get rekt");
+    }
+}
+
+console.log(game());
