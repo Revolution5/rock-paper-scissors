a8a4524f2488d4a3ff934bfe1b69ab2e13f900a2
Revolution5 <damonboone99@gmail.com>
2022-04-01 16:37:25 -0700

    Change logic in playRound() and show winner

--- rps.js
+++ rps.js
@@ -4,6 +4,9 @@ function computerPlay() {
     return randomChoice;
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 const resultsContainer = document.querySelector(".results-container");
 
 const youPlayed = document.createElement("p");
@@ -16,98 +19,84 @@ const outcome = document.createElement("p");
 resultsContainer.appendChild(outcome);
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection.toLowerCase();
-
     youPlayed.textContent = "You played " + playerSelection + "!";
     theyPlayed.textContent = "Your opponent played " + computerSelection + "!";
 
     switch(true) {
         case playerSelection == "rock" && computerSelection == "rock":
             outcome.textContent = ("It's a tie!");
-            return "tie";
             break;
         case playerSelection == "rock" && computerSelection == "paper":
             outcome.textContent = ("You lost the round! Paper beats rock");
-            return "lose";
+            computerScore++;
             break;
         case playerSelection == "rock" && computerSelection == "scissors":
             outcome.textContent = ("You Won the round! Rock beats scissors");
-            return "win";
+            playerScore++;
             break;
         case playerSelection == "paper" && computerSelection == "rock":
             outcome.textContent = ("You Won the round! Paper beats rock");
-            return "win";
+            playerScore++;
             break;
         case playerSelection == "paper" && computerSelection == "paper":
             outcome.textContent = ("It's a tie!");
-            return "tie";
             break;
         case playerSelection == "paper" && computerSelection == "scissors":
             outcome.textContent = ("You lost the round! Scissors beat paper");
-            return "lose";
+            computerScore++;
             break;
         case playerSelection == "scissors" && computerSelection == "rock":
             outcome.textContent = ("You lost the round! Rock beats scissors");
-            return "lose";
+            computerScore++;
             break;
         case playerSelection == "scissors" && computerSelection == "paper":
             outcome.textContent = ("You won the round! Scissors beat paper");
-            return "win";
+            playerScore++;
             break;
         case playerSelection == "scissors" && computerSelection == "scissors":
             outcome.textContent = ("It's a tie!");
-            return "tie";
             break;
     }
 }
 
-let playerScore = 0;
-let computerScore = 0;
 
 const btnrock = document.querySelector(".btnrock");
 const btnpaper = document.querySelector(".btnpaper");
 const btnscissors = document.querySelector(".btnscissors");
 
 const scoreContainer = document.querySelector(".score-container");
+
 const playerScoreCounter = document.createElement("p");
 scoreContainer.appendChild(playerScoreCounter);
+
 const computerScoreCounter = document.createElement("p");
 scoreContainer.appendChild(computerScoreCounter);
-
-function displayResult(result)
-{
-    if (result == "win") {
-        playerScore += 1;
-    }
-    else if (result == "lose") {
-        computerScore += 1;
-    }
-    playerScoreCounter.textContent = ("Player Score: " + playerScore);
-    computerScoreCounter.textContent = ("Computer Score: " + computerScore);
-}
-btnrock.addEventListener("click", function(event) {
-    let result = playRound("rock", computerPlay());
-    displayResult(result);
-})
-
-btnpaper.addEventListener("click", function(event) {
-    let result = playRound("paper", computerPlay());
-    displayResult(result);
-})
-
-btnscissors.addEventListener("click", function(event) {
-    let result = playRound("scissors", computerPlay());
-    displayResult(result);
-})
 
 const gameOver = document.createElement("p");
 scoreContainer.appendChild(gameOver);
 
-if (playerScore == 5 && computerScore < 5) {
-    gameOver.textContent = ("YOU WIN!!!");
-    
-}
-else if (computerScore == 5 && playerScore < 5) {
-    gameOver.textContent = ("YOU LOSE!!! GET REKT");
+function displayGameOver(){
+    playerScoreCounter.textContent = ("Player Score: " + playerScore);
+    computerScoreCounter.textContent = ("Computer Score: " + computerScore);
+    if (playerScore == 5 && computerScore < 5) {
+        gameOver.textContent = ("YOU WIN!!!");  
+    }
+    else if (computerScore == 5 && playerScore < 5) {
+        gameOver.textContent = ("YOU LOSE!!! GET REKT");
+    }
 }
 
+btnrock.addEventListener("click", function(event) {
+    playRound("rock", computerPlay());
+    displayGameOver();
+})
+
+btnpaper.addEventListener("click", function(event) {
+    playRound("paper", computerPlay());
+    displayGameOver();
+})
+
+btnscissors.addEventListener("click", function(event) {
+    playRound("scissors", computerPlay());
+    displayGameOver();
+})
